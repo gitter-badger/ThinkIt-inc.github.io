78efff7b67190a0f78aa2388550223866341ea9d
Joshua Zenn <wordman05@gmail.com>
2015-05-18 14:02:23 -0400

    Update requestsm.js

--- src/js/requestsm.js
+++ src/js/requestsm.js
@@ -1,1 +1,4 @@
-alert('There was an error submitting your request, please try again later.');
+function sendrequest()
+{
+  alert('There was an error submitting your request, please try again later.');
+}
