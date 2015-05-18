bd4e8d5e5d63b8fc645f3d91633ae7f10d70ee82
Joshua Zenn <wordman05@gmail.com>
2015-05-18 14:43:03 -0400

    Update requestsm.js

--- src/js/requestsm.js
+++ src/js/requestsm.js
@@ -1,3 +1,11 @@
 function sendrequest() {
-  alert('There was an error submitting your request, please try again later.');
+  isDebug = 1;
+  if(isDebug == 1) {
+    alert('There was an error submitting your request, please try again later.');
+  }
+  else
+  {
+    //Insert form submission stuff here
+    //SQL is too dangerous, try POSTint or GETing to a PHP or ASPX page
+  }
 }
